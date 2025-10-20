8e18a0cb1ae91f7d2954123026cce7fd6a1217be
Kylea Jackson <knjackson30974@users.noreply.github.com>
2025-10-20 14:11:57 -0300

    Update SVGBlacklistPreProcessor.php
    removing comment sections

--- src/FileUpload/Processor/SVGBlacklistPreProcessor.php
+++ src/FileUpload/Processor/SVGBlacklistPreProcessor.php
@@ -197,20 +197,13 @@ final class SVGBlacklistPreProcessor implements PreProcessor
 
     private function hasContentScriptTag(string $raw_svg_content): bool
     {
-<<<<<<< Updated upstream
         // JKN PATCH START
-        // // Check for Base64 encoded Content
-=======
         // Check for Base64 encoded Content
->>>>>>> Stashed changes
         // if (preg_match(self::REGEX_BASE64, $raw_svg_content)) {
         //     $this->rejection_message .= ' ' . $this->rejection_message_base64;
         //     return true;
         // }
-<<<<<<< Updated upstream
         // JKN PATCH END
-=======
->>>>>>> Stashed changes
 
         // Check for script tags directly
         if (preg_match(self::REGEX_SCRIPT, $raw_svg_content)) {
